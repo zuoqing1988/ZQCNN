609a5b7e8ce03e00a11307d6617486245aa3959b
Zuo Qing <zuoqing1988@aliyun.com>
2018-10-31 12:29:40 +0800

    更新Pnet20模型

--- SamplesZQCNN/SampleMTCNN/SampleMTCNN.cpp
+++ SamplesZQCNN/SampleMTCNN/SampleMTCNN.cpp
@@ -93,7 +93,7 @@ int main()
 	result_name = "resultdet.jpg";
 	if (use_pnet20)
 	{
-		if (!mtcnn.Init("model\\det1-dw20.zqparams", "model\\det1-dw20-7.nchwbin",
+		if (!mtcnn.Init("model\\det1-dw20.zqparams", "model\\det1-dw20-13.nchwbin",
 			"model\\det2.zqparams", "model\\det2_bgr.nchwbin",
 			"model\\det3.zqparams", "model\\det3_bgr.nchwbin", thread_num))
 		{
