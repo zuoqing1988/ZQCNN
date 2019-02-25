ef941579c9865e07236c975f57964cf5cc9450ee
Zuo Qing <zuoqing1988@aliyun.com>
2019-02-25 17:40:05 +0800

    尝试支持arm_neon

--- ZQCNN/layers_c/zq_cnn_innerproduct_gemm_32f_align_c.c
+++ ZQCNN/layers_c/zq_cnn_innerproduct_gemm_32f_align_c.c
@@ -3,6 +3,9 @@
 #include <string.h>
 #include <omp.h>
 #include "../ZQ_CNN_CompileConfig.h"
+#if __ARM_NEON
+#include <arm_neon.h>
+#else
 #if defined(__GNUC__)
 #if ZQ_CNN_USE_SSETYPE >= ZQ_CNN_SSETYPE_SSE
 #include <smmintrin.h>
@@ -26,7 +29,7 @@
 #include <intrin.h>//(include immintrin.h)  
 #endif
 #endif
-
+#endif //__ARM_NEON
 #include"math/zq_gemm_32f_align_c.h"
 
 #if ZQ_CNN_USE_BLAS_GEMM
@@ -38,6 +41,55 @@
 extern "C" {
 #endif
 
+#if __ARM_NEON
+#define zq_cnn_innerproduct_gemm_32f_align_same_pixstep zq_cnn_innerproduct_gemm_32f_align128bit_same_pixstep
+#define zq_cnn_innerproduct_gemm_32f_align_same_pixstep_batch zq_cnn_innerproduct_gemm_32f_align128bit_same_pixstep_batch
+#define zq_mm_load_ps vld1q_f32
+#define zq_mm_store_ps vst1q_f32
+#define zq_mm_add_ps vaddq_f32
+#if ZQ_CNN_USE_FMADD128
+#define zq_mm_fmadd_ps vfmaq_f32
+#else
+#define zq_mm_fmadd_ps(A, B, C) vaddq_f32(vmulq_f32(A, B), C)
+#endif
+#define zq_mm_mul_ps vmulq_f32
+#define zq_mm_setzero_ps() vdupq_n_f32(0)
+#define zq_mm_type float32x4_t
+#define zq_mm_align_size 4
+#define zq_mm_bitor_longlong 0xFFFFFFFFFFFFFFF0
+#define zq_final_sum_q (q[0]+q[1]+q[2]+q[3])
+
+#if (ZQ_CNN_USE_BLAS_GEMM || ZQ_CNN_USE_MKL_GEMM)
+#define	zq_cblas_sgemm cblas_sgemm
+#define zq_CblasRowMajor CblasRowMajor
+#define zq_CblasNoTrans CblasNoTrans
+#define zq_CblasTrans CblasTrans
+#else
+#define zq_CblasRowMajor 1
+#define zq_CblasNoTrans 1
+#define zq_CblasTrans 1
+#define	zq_cblas_sgemm(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14)  \
+      zq_gemm_32f_AnoTrans_Btrans_auto(x4,x5,x6,x8,x9,x10,x11,x13,x14)  
+#endif
+#include "zq_cnn_innerproduct_gemm_32f_align_c_raw.h"
+#undef zq_cblas_sgemm
+#undef zq_CblasRowMajor
+#undef zq_CblasNoTrans
+#undef zq_CblasTrans
+
+#undef zq_cnn_innerproduct_gemm_32f_align_same_pixstep
+#undef zq_cnn_innerproduct_gemm_32f_align_same_pixstep_batch
+#undef zq_mm_load_ps
+#undef zq_mm_store_ps
+#undef zq_mm_add_ps
+#undef zq_mm_fmadd_ps
+#undef zq_mm_mul_ps
+#undef zq_mm_setzero_ps
+#undef zq_mm_type
+#undef zq_mm_align_size
+#undef zq_mm_bitor_longlong
+#undef zq_final_sum_q
+#else
 #if ZQ_CNN_USE_SSETYPE >= ZQ_CNN_SSETYPE_SSE
 #define zq_cnn_innerproduct_gemm_32f_align_same_pixstep zq_cnn_innerproduct_gemm_32f_align128bit_same_pixstep
 #define zq_cnn_innerproduct_gemm_32f_align_same_pixstep_batch zq_cnn_innerproduct_gemm_32f_align128bit_same_pixstep_batch
@@ -140,6 +192,8 @@ extern "C" {
 #undef zq_final_sum_q
 
 #endif
+
+#endif //__ARM_NEON
 
 #if defined(__cplusplus) || defined(c_plusplus) 
 }
