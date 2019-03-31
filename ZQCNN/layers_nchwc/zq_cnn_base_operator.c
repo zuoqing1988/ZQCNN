9f911a6109cd99041f140da395df10426aa2c02a
左庆 <zuoqing1988@qq.com>
2019-03-31 18:04:14 +0800

    FIX BUG

--- ZQCNN/layers_nchwc/zq_cnn_base_operator.c
+++ ZQCNN/layers_nchwc/zq_cnn_base_operator.c
@@ -1,10 +1,10 @@
-inline float my_mm_load_ps(const float* ptr) { return *ptr; }
-inline void my_mm_store_ps(float* ptr, float val) { *ptr = val; }
-inline float my_mm_add_ps(float a, float b) { return a + b; }
-inline float my_mm_sub_ps(float a, float b) { return a - b; }
-inline float my_mm_mul_ps(float a, float b) { return a * b; }
-inline float my_mm_fmadd_ps(float a, float b, float c) { return a*b + c; }
-inline float my_mm_max_ps(float a, float b) { return a > b ? a : b; }
-inline float my_mm_min_ps(float a, float b) { return a < b ? a : b; }
-inline float my_mm_setzero_ps() { return 0; }
-inline float my_mm_set1_ps(float v) { return v; }
+float my_mm_load_ps(const float* ptr) { return *ptr; }
+void my_mm_store_ps(float* ptr, float val) { *ptr = val; }
+float my_mm_add_ps(float a, float b) { return a + b; }
+float my_mm_sub_ps(float a, float b) { return a - b; }
+float my_mm_mul_ps(float a, float b) { return a * b; }
+float my_mm_fmadd_ps(float a, float b, float c) { return a*b + c; }
+float my_mm_max_ps(float a, float b) { return a > b ? a : b; }
+float my_mm_min_ps(float a, float b) { return a < b ? a : b; }
+float my_mm_setzero_ps() { return 0; }
+float my_mm_set1_ps(float v) { return v; }
