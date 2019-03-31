e5cb680720ba4b33a02e5efcbe69df5de6e3e58d
左庆 <zuoqing1988@qq.com>
2019-03-31 18:05:24 +0800

    FIX BUG

--- ZQCNN/layers_nchwc/zq_cnn_base_operator.h
+++ ZQCNN/layers_nchwc/zq_cnn_base_operator.h
@@ -1,13 +1,13 @@
 #ifndef _ZQ_CNN_BASE_OPERATOR_H_
 #define _ZQ_CNN_BASE_OPERATOR_H_
-inline float my_mm_load_ps(const float* ptr);
-inline void my_mm_store_ps(float* ptr, float val);
-inline float my_mm_add_ps(float a, float b);
-inline float my_mm_sub_ps(float a, float b);
-inline float my_mm_mul_ps(float a, float b);
-inline float my_mm_fmadd_ps(float a, float b, float c);
-inline float my_mm_max_ps(float a, float b);
-inline float my_mm_min_ps(float a, float b);
-inline float my_mm_setzero_ps();
-inline float my_mm_set1_ps(float v);
+float my_mm_load_ps(const float* ptr);
+void my_mm_store_ps(float* ptr, float val);
+float my_mm_add_ps(float a, float b);
+float my_mm_sub_ps(float a, float b);
+float my_mm_mul_ps(float a, float b);
+float my_mm_fmadd_ps(float a, float b, float c);
+float my_mm_max_ps(float a, float b);
+float my_mm_min_ps(float a, float b);
+float my_mm_setzero_ps();
+float my_mm_set1_ps(float v);
 #endif
