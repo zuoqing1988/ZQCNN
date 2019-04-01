9b9f45d3ca279044a953e0a8fa69d962dd282824
Zuo Qing <zuoqing1988@aliyun.com>
2019-04-01 12:43:34 +0800

    更新Sample

--- SamplesZQCNN/SampleLnet106/SampleLnet106.cpp
+++ SamplesZQCNN/SampleLnet106/SampleLnet106.cpp
@@ -1,4 +1,5 @@
 #include "ZQ_CNN_Net.h"
+#include "ZQ_CNN_Net_NCHWC.h"
 #if defined(_WIN32)
 #include "ZQlib/ZQ_PutTextCN.h"
 #endif
@@ -34,12 +35,25 @@ int main()
 	mkl_set_num_threads(num_threads);
 #endif
 	int show_H = 640, show_W = 640;
-	ZQ_CNN_Net net1, net2;
-#if defined(_WIN32)
-	if (!net1.LoadFrom("model/det5-dw96-v2s.zqparams", "model/det5-dw96-v2s.nchwbin",true,1e-9)
-		|| !net2.LoadFrom("model/det3.zqparams", "model/det3_bgr.nchwbin", true, 1e-9))
+	ZQ_CNN_Net net1; 
+#if __ARM_NEON
+	ZQ_CNN_Net_NCHWC<ZQ_CNN_Tensor4D_NCHWC4> net2;
 #else
-	if (!net1.LoadFrom("../../model/det5-dw96-v2s.zqparams", "../../model/det5-dw96-v2s.nchwbin", true, 1e-9)
+#if ZQ_CNN_USE_SSETYPE >= ZQ_CNN_SSETYPE_AVX
+	ZQ_CNN_Net_NCHWC<ZQ_CNN_Tensor4D_NCHWC8> net2;
+#elif ZQ_CNN_USE_SSETYPE >= ZQ_CNN_SSETYPE_SSE
+	ZQ_CNN_Net_NCHWC<ZQ_CNN_Tensor4D_NCHWC4> net2;
+#else
+	ZQ_CNN_Net_NCHWC<ZQ_CNN_Tensor4D_NCHWC1> net2;
+#endif
+#endif
+	
+#if defined(_WIN32)
+	if (!net1.LoadFrom("model/det5-dw96-v2s.zqparams", "model/det5-dw96-v2s-8000.nchwbin",true,1e-9,true)
+		//|| !net2.LoadFrom("model/det3.zqparams", "model/det3_bgr.nchwbin", true, 1e-9))
+		|| !net2.LoadFrom("model/det5-dw96-v2s.zqparams", "model/det5-dw96-v2s-8000.nchwbin", true, 1e-9, true))
+#else
+	if (!net1.LoadFrom("../../model/det5-dw96-v2s.zqparams", "../../model/det5-dw96-v2s-8000.nchwbin", true, 1e-9)
 		|| !net2.LoadFrom("../../model/det3.zqparams", "../../model/det3_bgr.nchwbin", true, 1e-9))
 #endif
 	{
@@ -70,40 +84,66 @@ int main()
 	Mat draw_img;
 	cv::resize(img, draw_img, cv::Size(show_W, show_H));
 
-	ZQ_CNN_Tensor4D_NHW_C_Align128bit input1, input2;
+	ZQ_CNN_Tensor4D_NHW_C_Align128bit input1;
+#if __ARM_NEON 
+	ZQ_CNN_Tensor4D_NCHWC4 input2;
+#else
+#if ZQ_CNN_USE_SSETYPE >= ZQ_CNN_SSETYPE_AVX
+	ZQ_CNN_Tensor4D_NCHWC8 input2;
+#elif ZQ_CNN_USE_SSETYPE >= ZQ_CNN_SSETYPE_SSE
+	ZQ_CNN_Tensor4D_NCHWC4 input2;
+#else
+	ZQ_CNN_Tensor4D_NCHWC1 input2;
+#endif
+#endif
+
 	input1.ConvertFromBGR(img1.data, img1.cols, img1.rows, img1.step[0]);
 	input2.ConvertFromBGR(img2.data, img2.cols, img2.rows, img2.step[0]);
-	int nIters = 1000;
-	double t1 = omp_get_wtime();
-	for (int i = 0; i < nIters; i++)
-		net1.Forward(input1);
-	double t2 = omp_get_wtime();
-	for (int i = 0; i < nIters; i++)
-		net2.Forward(input2);
-	double t3 = omp_get_wtime();
-	printf("net1 %.3f s / %d = %.3f ms\n", t2 - t1, nIters, 1000 * (t2 - t1) / nIters);
-	printf("net2 %.3f s / %d = %.3f ms\n", t3 - t2, nIters, 1000 * (t3 - t2) / nIters);
-	printf("last time of net1:\n conv = %.3f ms, dwonv = %.3f ms, bns = %.3f ms, prelu = %.3f ms\n",
-		1000 * net1.GetLastTimeOfLayerType("Convolution"),
-		1000 * net1.GetLastTimeOfLayerType("DepthwiseConvolution"),
-		1000 * net1.GetLastTimeOfLayerType("BatchNormScale"),
-		1000 * net1.GetLastTimeOfLayerType("PReLU")
-	);
-	printf("last time of net2:\n conv = %.3f ms, dwonv = %.3f ms, bns = %.3f ms, prelu = %.3f ms\n",
-		1000 * net2.GetLastTimeOfLayerType("Convolution"),
-		1000 * net2.GetLastTimeOfLayerType("DepthwiseConvolution"),
-		1000 * net2.GetLastTimeOfLayerType("BatchNormScale"),
-		1000 * net2.GetLastTimeOfLayerType("PReLU")
-	);
+	for (int out_it = 0; out_it < 3; out_it++)
+	{
+		int nIters = 10000;
+		double t1 = omp_get_wtime();
+		for (int i = 0; i < nIters; i++)
+			net1.Forward(input1);
+		double t2 = omp_get_wtime();
+		for (int i = 0; i < nIters; i++)
+			net2.Forward(input2);
+		double t3 = omp_get_wtime();
+		printf("net1 %.3f s / %d = %.3f ms\n", t2 - t1, nIters, 1000 * (t2 - t1) / nIters);
+		printf("net2 %.3f s / %d = %.3f ms\n", t3 - t2, nIters, 1000 * (t3 - t2) / nIters);
+		printf("last time of net1:\n conv = %.3f ms, dwonv = %.3f ms, bns = %.3f ms, prelu = %.3f ms\n",
+			1000 * net1.GetLastTimeOfLayerType("Convolution"),
+			1000 * net1.GetLastTimeOfLayerType("DepthwiseConvolution"),
+			1000 * net1.GetLastTimeOfLayerType("BatchNormScale"),
+			1000 * net1.GetLastTimeOfLayerType("PReLU")
+		);
+		printf("last time of net2:\n conv = %.3f ms, dwonv = %.3f ms, bns = %.3f ms, prelu = %.3f ms\n",
+			1000 * net2.GetLastTimeOfLayerType("Convolution"),
+			1000 * net2.GetLastTimeOfLayerType("DepthwiseConvolution"),
+			1000 * net2.GetLastTimeOfLayerType("BatchNormScale"),
+			1000 * net2.GetLastTimeOfLayerType("PReLU")
+		);
+	}
 	const ZQ_CNN_Tensor4D* landmark1 = net1.GetBlobByName("conv6-3");
-	const ZQ_CNN_Tensor4D* landmark2 = net2.GetBlobByName("conv6-3");
+#if __ARM_NEON 
+	const ZQ_CNN_Tensor4D_NCHWC4* landmark2 = net2.GetBlobByName("conv6-3");
+#else
+#if ZQ_CNN_USE_SSETYPE >= ZQ_CNN_SSETYPE_AVX
+	const ZQ_CNN_Tensor4D_NCHWC8* landmark2 = net2.GetBlobByName("conv6-3");
+#elif ZQ_CNN_USE_SSETYPE >= ZQ_CNN_SSETYPE_SSE
+	const ZQ_CNN_Tensor4D_NCHWC4* landmark2 = net2.GetBlobByName("conv6-3");
+#else
+	const ZQ_CNN_Tensor4D_NCHWC1* landmark2 = net2.GetBlobByName("conv6-3");
+#endif
+#endif
+	
 	if (landmark1 == 0 || landmark2 == 0)
 	{
 		cout << "failed to get blob conv6-3\n";
 		return EXIT_FAILURE;
 	}
-	const float* landmark1_data = landmark1->GetFirstPixelPtr();
-	const float* landmark2_data = landmark2->GetFirstPixelPtr();
+	const float* landmark2_data = landmark1->GetFirstPixelPtr();
+	const float* landmark1_data = landmark2->GetFirstPixelPtr();
 	for (int i = 0; i < 106; i++)
 	{
 		char buf[10];
