3c66f6a27576dba161941bc8eeeef69b5dea8c52
Zuo Qing <zuoqing1988@aliyun.com>
2019-04-18 09:11:18 +0800

    FIX BUG

--- ZQCNN/ZQ_CNN_Forward_SSEUtils_NCHWC.cpp
+++ ZQCNN/ZQ_CNN_Forward_SSEUtils_NCHWC.cpp
@@ -1969,8 +1969,8 @@ bool ZQ_CNN_Forward_SSEUtils_NCHWC::ConvolutionWithBiasPReLU(ZQ_CNN_Tensor4D_NCH
 
 	if (filter_H == 1 && filter_W == 1)
 	{
-#if __ARM_NEON&&__ARM_NEON_ARMV8 && 0
-		zq_cnn_convolution_gemm_nchwc4_packedM6N4_kernel1x1_with_bias_prelu(in_firstPixelData, in_N, in_H, in_W, in_C,
+#if __ARM_NEON&&__ARM_NEON_ARMV8 && 1
+		zq_cnn_convolution_gemm_nchwc4_packedM4N8_other_kernel1x1_with_bias_prelu(in_firstPixelData, in_N, in_H, in_W, in_C,
 			in_widthStep, in_sliceStep, in_imStep, (const float*)(packedfilters.data),
 			out_firstPixelData, need_N, need_H, need_W, need_C,
 			out_widthStep, out_sliceStep, out_imStep, bias_firstPixelData, slope_firstPixelData, buffer, buffer_len);
@@ -2047,8 +2047,8 @@ bool ZQ_CNN_Forward_SSEUtils_NCHWC::ConvolutionWithPReLU(ZQ_CNN_Tensor4D_NCHWC4&
 
 	if (filter_H == 1 && filter_W == 1)
 	{
-#if __ARM_NEON && __ARM_NEON_ARMV8 && 0
-		zq_cnn_convolution_gemm_nchwc4_packedM6N4_kernel1x1(in_firstPixelData, in_N, in_H, in_W, in_C,
+#if __ARM_NEON && __ARM_NEON_ARMV8 && 1
+		zq_cnn_convolution_gemm_nchwc4_packedM4N8_other_kernel1x1(in_firstPixelData, in_N, in_H, in_W, in_C,
 			in_widthStep, in_sliceStep, in_imStep, (const float*)(packedfilters.data),
 			out_firstPixelData, need_N, need_H, need_W, need_C,
 			out_widthStep, out_sliceStep, out_imStep, buffer, buffer_len);
@@ -2124,8 +2124,8 @@ bool ZQ_CNN_Forward_SSEUtils_NCHWC::Convolution(ZQ_CNN_Tensor4D_NCHWC4& input,
 	
 	if (filter_H == 1 && filter_W == 1)
 	{
-#if __ARM_NEON && __ARM_NEON_ARMV8 && 0
-		zq_cnn_convolution_gemm_nchwc4_packedM6N4_kernel1x1(in_firstPixelData, in_N, in_H, in_W, in_C,
+#if __ARM_NEON && __ARM_NEON_ARMV8 && 1
+		zq_cnn_convolution_gemm_nchwc4_packedM4N8_other_kernel1x1(in_firstPixelData, in_N, in_H, in_W, in_C,
 			in_widthStep, in_sliceStep, in_imStep, (const float*)(packedfilters.data),
 			out_firstPixelData, need_N, need_H, need_W, need_C,
 			out_widthStep, out_sliceStep, out_imStep, buffer, buffer_len);
