7ba8e9bc2828f98f559a3a95509cdf527b792a5d
Zuo Qing <zuoqing1988@aliyun.com>
2019-04-18 12:51:52 +0800

    尝试优化conv

--- ZQCNN/ZQ_CNN_Layer_NCHWC.h
+++ ZQCNN/ZQ_CNN_Layer_NCHWC.h
@@ -293,7 +293,7 @@ namespace ZQ
 					__int64* tmp_buffer_len = ZQ_CNN_Layer_NCHWC<Tensor4D>::use_buffer ? ZQ_CNN_Layer_NCHWC<Tensor4D>::buffer_len : 0;
 					bool ret = false;
 #if __ARM_NEON
-					//if (kernel_H == 1 && kernel_W == 1)
+					if ((kernel_H == 1 && kernel_W == 1) || filters->GetN() <= 32)
 					ret = ZQ_CNN_Forward_SSEUtils_NCHWC::ConvolutionWithBiasPReLU(*((*bottoms)[0]),
 						packedfilters, filters->GetN(), filters->GetH(), filters->GetW(), filters->GetC(),
 						*bias, *prelu_slope, stride_H, stride_W, dilate_H, dilate_W, pad_H, pad_W, *((*tops)[0]),
@@ -328,7 +328,7 @@ namespace ZQ
 					__int64* tmp_buffer_len = ZQ_CNN_Layer_NCHWC<Tensor4D>::use_buffer ? ZQ_CNN_Layer_NCHWC<Tensor4D>::buffer_len : 0;
 					bool ret = false;
 #if __ARM_NEON
-					//if (kernel_H == 1 && kernel_W == 1)
+					if ((kernel_H == 1 && kernel_W == 1) || filters->GetN() <= 32)
 					ret = ZQ_CNN_Forward_SSEUtils_NCHWC::ConvolutionWithBias(*((*bottoms)[0]),
 						packedfilters, filters->GetN(), filters->GetH(), filters->GetW(), filters->GetC(),
 						*bias, stride_H, stride_W, dilate_H, dilate_W, pad_H, pad_W, *((*tops)[0]),
@@ -365,7 +365,7 @@ namespace ZQ
 					__int64* tmp_buffer_len = ZQ_CNN_Layer_NCHWC<Tensor4D>::use_buffer ? ZQ_CNN_Layer_NCHWC<Tensor4D>::buffer_len : 0;
 					bool ret = false;
 #if __ARM_NEON
-					//if(kernel_H == 1 && kernel_W == 1)
+					if ((kernel_H == 1 && kernel_W == 1) || filters->GetN() <= 32)
 					ret = ZQ_CNN_Forward_SSEUtils_NCHWC::ConvolutionWithPReLU(*((*bottoms)[0]),
 						packedfilters, filters->GetN(), filters->GetH(), filters->GetW(), filters->GetC(),
 						*prelu_slope, stride_H, stride_W, dilate_H, dilate_W, pad_H, pad_W, *((*tops)[0]),
@@ -398,7 +398,7 @@ namespace ZQ
 					__int64* tmp_buffer_len = ZQ_CNN_Layer_NCHWC<Tensor4D>::use_buffer ? ZQ_CNN_Layer_NCHWC<Tensor4D>::buffer_len : 0;
 					bool ret = false;
 #if __ARM_NEON
-					//if (kernel_H == 1 && kernel_W == 1)
+					if ((kernel_H == 1 && kernel_W == 1) || filters->GetN() <= 32)
 					ret = ZQ_CNN_Forward_SSEUtils_NCHWC::Convolution(*((*bottoms)[0]),
 						packedfilters, filters->GetN(), filters->GetH(), filters->GetW(), filters->GetC(),
 						stride_H, stride_W, dilate_H, dilate_W, pad_H, pad_W, *((*tops)[0]),
